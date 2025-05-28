3003f318129d5c3180b474306abbab64fcaeb974
Maxim Papko <papkom50@gmail.com>
2025-05-28 21:49:17 +0300

    add a properties to Currency model at Currency.cs

--- CryptoViewer/Models/Currency.cs
+++ CryptoViewer/Models/Currency.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,17 @@ namespace CryptoViewer.Models
 {
     class Currency
     {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public double CurrentPrice { get; set; }
+        public double MarketCap { get; set; }
+        public double PriceChangePercentageFor24H { get; set; }
+
+
+
+
+
     }
 }
