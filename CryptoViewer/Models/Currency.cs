af0ec38973313a983f09b3a48cd5037b4ca79d83
Maxim Papko <papkom50@gmail.com>
2025-05-30 16:16:58 +0300

    fix an actual price reflection for each coin

--- CryptoViewer/Models/Currency.cs
+++ CryptoViewer/Models/Currency.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.RightsManagement;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CryptoViewer.Models
 {
     public class Currency
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
+
+        [JsonPropertyName("symbol")]
         public string Symbol { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
+
+        [JsonPropertyName("current_price")]
         public decimal CurrentPrice { get; set; }
 
+
+        [JsonPropertyName("market_cap")]
         public decimal MarketCap { get; set; }
+
+        [JsonPropertyName("total_volume")]
         public decimal TotalVolume { get; set; }
 
+
+        [JsonPropertyName("price_change_percentage_24h")]
         public decimal PriceChangePercentage24h { get; set; }
 
+        [JsonPropertyName("image")]
         public string Image { get; set; }
 
 
