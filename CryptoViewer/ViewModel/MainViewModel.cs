59bc5265e6df3ab01a34971c71b9b794db31fa46
Maxim Papko <papkom50@gmail.com>
2025-05-30 14:58:59 +0300

    made a static coins without api requests

--- CryptoViewer/ViewModel/MainViewModel.cs
+++ CryptoViewer/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using CryptoViewer.Models;
 using CryptoViewer.Services;
@@ -8,27 +9,43 @@ namespace CryptoViewer.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private ObservableCollection<Currency> _currencies;
-        public ObservableCollection<Currency> Currencies
-        {
-            get => _currencies;
-            set { _currencies = value; OnPropertyChanged(nameof(Currencies)); }
-        }
-
+        public ObservableCollection<Currency> Currencies { get; set; }
         public MainViewModel()
         {
-            LoadCurrencies();
+            // Тимчасові дані для перевірки
+            Currencies = new ObservableCollection<Currency>
+            {
+                new Currency { Name = "Bitcoin", Symbol = "BTC", CurrentPrice = 64000 },
+                new Currency { Name = "Ethereum", Symbol = "ETH", CurrentPrice = 3200 }
+            };
         }
 
-        private async void LoadCurrencies()
-        {
-            var list = await CoinGeckoService.GetTopCurrenciesAsync();
-            Currencies = new ObservableCollection<Currency>(list);
-        }
-
-        protected void OnPropertyChanged(string name) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
+
+
+    /*public event PropertyChangedEventHandler PropertyChanged;
+
+    private ObservableCollection<Currency> _currencies;
+    public ObservableCollection<Currency> Currencies
+    {
+        get => _currencies;
+        set { _currencies = value; OnPropertyChanged(nameof(Currencies)); }
+    }
+
+    public MainViewModel()
+    {
+        LoadCurrencies();
+    }
+
+    private async void LoadCurrencies()
+    {
+        var list = await CoinGeckoService.GetTopCurrenciesAsync();
+        Currencies = new ObservableCollection<Currency>(list);
+    }
+
+    protected void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));*/
 }
